cd6f5844af6747b863c7664d76952f971094ff63
Marcin Kozlowski <martinkoaz@gmail.com>
2020-12-25 18:50:43 +0100

    Change project name. Init fix

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            new Swift().Connect("0d473b30fcbbded914e8d4cc0cc18bd7c2a5639dac04ce2137b66d4b6322c516e9c090cc80cebb611a8ff30e99a53e40610f5140fa58fd3cd840e3331d8d78b921");
+            var swiftClient = new Swift();
+            swiftClient.Init();
+            swiftClient.Connect("0d473b30fcbbded914e8d4cc0cc18bd7c2a5639dac04ce2137b66d4b6322c516e9c090cc80cebb611a8ff30e99a53e40610f5140fa58fd3cd840e3331d8d78b921");
+
             Console.ReadLine();
         }
     }
