3540487d5d3d47cd3b106bbafa3c85977382b24a
Marcin Kozlowski <martinkoaz@gmail.com>
2020-12-27 15:51:22 +0100

    Send request to all peers

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using SwiftCSharp.PPSP;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 
@@ -18,16 +19,27 @@ namespace ConsoleApp1
             Console.WriteLine("Tracker::Announce");
             // TODO: When iterating peers exclude own address, it might be on the peers list
             string swarmId = new ProgramGuide().GetAllChannels().List.First().ChannelList.First().SwarmId; // first category -> first channel; for testing only
-            PPSPTrackerProtocol trackerResult = swiftClient.Connect(swarmId).Container;
-            PeerAddrModel firstAvailablePeer = trackerResult.SwarmResult.First().PeerGroup.PeerInfo.Where(p => p.PeerAddr.IpAddress.Address != swiftClient.SwiftTracker.RemoteAddress.Address.ToString()).First().PeerAddr; // result -> swarmresult is always only one -> first reflexive peer from group; for testing only
-            IPEndPoint peerAddress = new IPEndPoint(IPAddress.Parse(firstAvailablePeer.IpAddress.Address), firstAvailablePeer.Port);
-
             Console.WriteLine($"SwarmID: {swarmId}");
-            Console.WriteLine($"PeerAddr: {peerAddress}");
+
+            List<PeerInfoModel> peersList = swiftClient.Connect(swarmId).Container.SwarmResult.First().PeerGroup.PeerInfo;
+
+            foreach (var peerInfo in peersList)
+            {
+                IPEndPoint peerAddress = new IPEndPoint(IPAddress.Parse(peerInfo.PeerAddr.IpAddress.Address), peerInfo.PeerAddr.Port);
+                Console.WriteLine($"Testing PeerAddr: {peerAddress}");
+
+                byte[] handshakeResult = new PpspClient(swarmId).SendHandshake(peerAddress, swiftClient.SwiftTracker.LocalAddress.Port);
+
+                if (handshakeResult == null)
+                {
+                    Console.WriteLine("Peer not connected");
+                } else
+                {
+                    Console.WriteLine("Peer is connected");
+                }
+            }
 
             // Send messages to peer and disconnect from tracker
-            byte[] handshakeResult = new PpspClient(swarmId).SendHandshake(peerAddress, swiftClient.SwiftTracker.LocalAddress.Port);
-            Console.WriteLine($"HANDSHAKE Result: {Helper.ByteArrayToString(handshakeResult)}");
             Console.WriteLine("Swift::Disconnect");
             swiftClient.Disconnect();
 
